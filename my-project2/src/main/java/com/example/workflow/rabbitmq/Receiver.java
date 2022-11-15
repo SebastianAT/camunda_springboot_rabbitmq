d72613fade01825cc9fd115534e51b94608dc932
Sebastian Trattnig <setr780@gmail.com>
2022-11-15 18:53:40 +0100

    Initial commit

---
+++ my-project2/src/main/java/com/example/workflow/rabbitmq/Receiver.java
@@ -0,0 +1,32 @@
+package com.example.workflow.rabbitmq;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.core.Response;
+import java.util.concurrent.CountDownLatch;
+
+@Component
+public class Receiver {
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    @Autowired
+    CamundaMessageProcessor messageProcessor;
+
+    public void receiveMessage(String message) {
+        try {
+            Response response = messageProcessor.processMessage(message);
+        }catch(Exception e){
+            // @TODO: Fix up error catching with the service
+            System.out.println(e.getMessage());
+        }finally{
+            latch.countDown();
+        }
+
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+}
